8a03439fa6aabc4aa4e2a820b123639c5929882d
nedas-sim <nedas.sim@gmail.com>
2022-10-15 10:56:36 +0300

    Finished up main page with no functionality so far

--- Mobile/src/features/authentication/LoginForm.js
+++ Mobile/src/features/authentication/LoginForm.js
@@ -4,18 +4,27 @@ import { SafeAreaView, StyleSheet, TextInput, Button } from "react-native";
 const LoginForm = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput style={styles.input} placeholder="Email" />
-      <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#fff"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#fff"
+        secureTextEntry
+        autoCapitalize="none"
+      />
       <Button title="Login" />
     </SafeAreaView>
   );
 };
 
-const debuging3 = { borderColor: "green", borderWidth: 1 };
-
 const styles = StyleSheet.create({
   container: {
-    ...debuging3,
     width: "70%",
     height: "40%",
     flexDirection: "column",
--- Mobile/src/screens/MainScreen.js
+++ Mobile/src/screens/MainScreen.js
@@ -1,32 +1,20 @@
 import React from "react";
-import { Text, SafeAreaView, View, Button, StyleSheet } from "react-native";
-import LoginForm from "../components/LoginForm";
+import { Text, View, Button, StyleSheet } from "react-native";
+import LoginForm from "../features/authentication/LoginForm";
 
 const MainScreen = () => {
-  /*return (
-    <SafeAreaView style={styles.screen}>
-      <View style={styles.container}>
-        <Text style={styles.appName}>Split The Bill</Text>
-        <View style={styles.buttonContainer}>
-          <Button style={[styles.button, styles.login]} title="Login" />
-          <Button style={[styles.button, styles.register]} title="Register" />
-        </View>
-      </View>
-    </SafeAreaView>
-  );*/
   return (
     <View style={styles.screen}>
       <View style={styles.container}>
         <Text style={styles.appName}>Split The Bill</Text>
         <LoginForm />
-        <View style={styles.extraButtonsContainer}></View>
+        <View style={styles.extraButtonsContainer}>
+          <Button title="Register" />
+        </View>
       </View>
     </View>
   );
 };
-
-const debuging1 = { borderColor: "red", borderWidth: 1 };
-const debuging2 = { borderColor: "green", borderWidth: 1 };
 
 const styles = StyleSheet.create({
   screen: {
@@ -37,16 +25,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   container: {
-    ...debuging1,
     width: "80%",
     height: "50%",
     alignItems: "center",
   },
   appName: {
-    //...debuging2,
     color: "#fff",
     height: "30%",
     fontSize: 35,
+  },
+  extraButtonsContainer: {
+    width: "70%",
+    marginTop: 25,
   },
 });
 
