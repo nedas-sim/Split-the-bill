760cfe8955ca372a09bf1d2d65186da56e81095a
nedas-sim <nedas.sim@gmail.com>
2022-10-15 19:22:41 +0300

    Deleted unnecessary file

--- Mobile/src/features/authentication/RegisterForm.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
-
-const RegisterForm = () => {
-  return <SafeAreaView>RegisterForm</SafeAreaView>;
-};
-
-export default RegisterForm;
