4d5767ee940950ee511a35174997e89ec52cb667
nedas-sim <nedas.sim@gmail.com>
2022-10-19 22:49:54 +0300

    Removed unnecessary fields for user group entity

--- Backend/SplitTheBill/Domain/Database/UserGroup.cs
+++ Backend/SplitTheBill/Domain/Database/UserGroup.cs
@@ -8,8 +8,6 @@ public sealed class UserGroup
     public Guid GroupId { get; set; }
     public Group Group { get; set; }
 
-    public bool IsAdmin { get; set; }
     public DateTime InvitedOn { get; set; }
     public DateTime? AcceptedOn { get; set; }
-    public DateTime? RejectedOn { get; set; }
 }
