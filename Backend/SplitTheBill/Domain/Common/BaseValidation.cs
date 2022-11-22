e1de9bb8f0e41cbf6e17c5dc1c6cef04658cb477
nedas-sim <nedas.sim@gmail.com>
2022-11-22 18:17:44 +0200

    Created abstract class

---
+++ Backend/SplitTheBill/Domain/Common/BaseValidation.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.Common;
+
+public abstract class BaseValidation
+{
+    public virtual bool IsValid(out string? errorMessage)
+    {
+        // By default the request is valid
+        errorMessage = null;
+        return true;
+    }
+
+    public void ValidateAndThrow()
+    {
+        if (IsValid(out string? errorMessage) is false)
+        {
+            throw new ValidationErrorException(errorMessage!);
+        }
+    }
+}
